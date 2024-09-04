eb74175aad59aec1ec161f6da5f386cbdb13ab48
Eduardo Flores <edfloreshz@gmail.com>
2024-09-04 11:51:00 -0700

    improv: general improvements

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,8 @@ mod config;
 mod i18n;
 
 fn main() -> cosmic::iced::Result {
-    // Get the system's preferred languages.
     let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
-
-    // Enable localizations to be applied.
     i18n::init(&requested_languages);
-
-    // Settings for configuring the application window and iced runtime.
     let settings = cosmic::app::Settings::default();
-
-    // Starts the application's event loop with `()` as the application's flags.
     cosmic::app::run::<app::AppModel>(settings, ())
 }
